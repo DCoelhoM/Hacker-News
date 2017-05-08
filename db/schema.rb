673284d919776a6e974f69fa863a304674c955fe
Dinis Coelho Marques <dinis.c.mrqs@gmail.com>
2017-05-08 10:56:03 +0100

    users signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170506122613) do
+ActiveRecord::Schema.define(version: 20170508094409) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "source"
     t.text "text"
     t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
