d5d8699859c657cb48eec710459bd2ada52bfd47
Dinis Coelho Marques <dinis.c.mrqs@gmail.com>
2017-05-09 14:20:18 +0100

    destroy article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :authorize, only: [:new, :create]
+  before_action :authorize, only: [:new, :create, :destroy]
   def index
     @articles = Article.all
   end
@@ -13,12 +13,20 @@ class ArticlesController < ApplicationController
   end
 
   def create
-      @article = Article.new(article_params.merge(:user_id => session[:user_id]))
-      if @article.save
-        redirect_to @article
-      else
-        render 'new'
-      end
+    @article = Article.new(article_params.merge(:user_id => session[:user_id]))
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    if @article.user_id == session[:user_id]
+      @article.destroy
+      redirect_to articles_path
+    end
   end
 
   private
